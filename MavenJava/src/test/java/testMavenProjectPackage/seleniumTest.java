34d6a4516131f02d0104e3a162a6c121305ada0a
Praveen <spraveen879@gmail.com>
2023-03-20 14:35:49 +0530

    Pushing the paramterization changes

--- MavenJava/src/test/java/testMavenProjectPackage/seleniumTest.java
+++ MavenJava/src/test/java/testMavenProjectPackage/seleniumTest.java
@@ -2,15 +2,18 @@ package testMavenProjectPackage;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
 public class seleniumTest {
 	
+	@Parameters("name")
 	@Test
-	public void seleniumPrint() {
+	public void seleniumPrint(String a) {
 		
 		System.out.println("Selenium");
+		System.out.println(a);
 		
 	}
 	
@@ -18,6 +21,7 @@ public class seleniumTest {
 	public void bfrMethod() {
 		
 		System.out.println("Before method is executed");
+		System.out.println(System.getProperty("name"));
 		
 	}
 
