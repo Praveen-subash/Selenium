c5174048a367cf7219c11ec55e557c7bc88b1699
Praveen yahoo <spraveen879@gmail.com>
2024-01-30 12:04:23 +0530

    changes

--- Selenium4_Introduction/src/test/java/Test.java
+++ Selenium4_Introduction/src/test/java/Test.java
@@ -27,6 +27,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
+		//
 		
 		EdgeOptions options= new EdgeOptions();
 		options.addArguments("incognito");
