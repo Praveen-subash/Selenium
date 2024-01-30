d051acd451ac3a0ac89df239211785b2fd39ec00
spraveen879 <124035928+spraveen879@users.noreply.github.com>
2024-01-30 22:22:42 +0530

    Update Test.java

--- Selenium4_Introduction/src/test/java/Test.java
+++ Selenium4_Introduction/src/test/java/Test.java
@@ -28,12 +28,12 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
 		
-		ChromeOptions options= new ChromeOptions();
+		EdgeOptions options= new EdgeOptions();
 		options.addArguments("incognito");
 		options.addArguments("--disable-geolocation");
 	    options.addArguments("disable-infobars");
 	    
-		WebDriver driver = new ChromeDriver(options);
+		WebDriver driver = new EdgeDriver(options);
 		driver.get("https://www.spicejet.com/");
 		
 		
