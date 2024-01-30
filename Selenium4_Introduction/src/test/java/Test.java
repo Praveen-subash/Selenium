d6406e17157f1d25c53aa8f3bdf4ff1b54d411a0
Praveen-subash <spraveen879@gmail.com>
2024-01-30 22:24:15 +0530

    Update Test.java
    Commit new zbr

--- Selenium4_Introduction/src/test/java/Test.java
+++ Selenium4_Introduction/src/test/java/Test.java
@@ -28,6 +28,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
 		
+		//THis is RS
+		
 		ChromeOptions options= new ChromeOptions();
 		options.addArguments("incognito");
 		options.addArguments("--disable-geolocation");
